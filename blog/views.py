e0636c8ead1b07b318ab4b689cc87eacdcc8f997
shuvo <shuvasishtalukdershuvoalways@gmail.com>
2023-07-13 02:10:20 +0600

    routing and html templates are done

--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,5 @@ def posts(request):
     return render(request, 'blog/all-posts.html')
 
 
-def post_details(request):
-    pass
+def post_details(request, slug):
+    return render(request, 'blog/post-details.html')
