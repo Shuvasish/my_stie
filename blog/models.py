9494397ab2ef85e7ae47f64a97fe79c627e18388
shuvo <shuvasishtalukdershuvoalways@gmail.com>
2023-07-18 21:45:49 +0600

    migration and admin loging setup

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,10 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -17,8 +19,8 @@ class Post(models.Model):
     excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
-    slug = models.SlugField(unique=True, bd_index=True)
+    slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     Author = models.ForeignKey(
-        Author, on_delete=models.SET_NULL, related_name="posts")
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
