9dfb2fad2c3aa319ebc52baf9985602a48ec389d
shuvo <shuvasishtalukdershuvoalways@gmail.com>
2023-07-25 19:23:30 +0600

    added comment features

--- blog/views.py
+++ blog/views.py
@@ -2,24 +2,26 @@ from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.views import View
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from .models import Post
-
+from .forms import CommentForm
 
 
 # Create your views here.
 
 class StartingPageView(ListView):
     template_name = 'blog/index.html'
-    model = Post 
+    model = Post
     ordering = ['-date']
     context_object_name = 'posts'
 
     def get_queryset(self):
-        queryset =  super().get_queryset()
+        queryset = super().get_queryset()
         data = queryset[:3]
-        return data 
-    
+        return data
 
 
 '''def starting_page(request):
@@ -27,6 +29,7 @@ class StartingPageView(ListView):
     return render(request, 'blog/index.html',{
         'posts': latest_posts
     })'''
+
 
 class AllPostsView(ListView):
     template_name = 'blog/all-posts.html'
@@ -41,14 +44,45 @@ class AllPostsView(ListView):
         'all_posts': all_posts
     })'''
 
-class SinglePostView(DetailView):
-    template_name = 'blog/post-details.html'
-    model = Post
 
-    def get_context_data(self, **kwargs):
+class SinglePostView(View):
+    # template_name = 'blog/post-details.html'
+    # model = Post
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm(),
+            'comments': post.comments.all().order_by('-id')
+        }
+        return render(request, 'blog/post-details.html', context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse('post-details-page', args=[slug]))
+
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': comment_form,
+            'comments': post.comments.all()
+        }
+        return render(request, 'blog/post-details.html', context)
+
+    '''def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['post_tags'] = self.object.tags.all()
-        return context
+        context['comment_form'] = CommentForm()
+        return context'''
 
 
 '''def post_details(request, slug):
